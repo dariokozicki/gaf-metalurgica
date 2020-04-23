60e72cf0b23b70f2286c331457e5b584de9e6572
Dario <dariokozicki@gmail.com>
2020-04-22 23:05:58 -0300

    Updates

---
+++ precache-manifest.7eb729cd84630dd40edf668e1c9115cc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a826a827b1b73417eea6fbaf41f89db",
+    "url": "/gaf-metalurgica/index.html"
+  },
+  {
+    "revision": "4865d8b798baff5dc9ef",
+    "url": "/gaf-metalurgica/static/css/2.14420a7e.chunk.css"
+  },
+  {
+    "revision": "22b4e59e3fe4ac0260be",
+    "url": "/gaf-metalurgica/static/css/main.6fa67284.chunk.css"
+  },
+  {
+    "revision": "4865d8b798baff5dc9ef",
+    "url": "/gaf-metalurgica/static/js/2.33218901.chunk.js"
+  },
+  {
+    "revision": "78e572a38ce62fad1af1cd75e03f15bb",
+    "url": "/gaf-metalurgica/static/js/2.33218901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b4e59e3fe4ac0260be",
+    "url": "/gaf-metalurgica/static/js/main.d101f9a0.chunk.js"
+  },
+  {
+    "revision": "81dc2192441195bbc451",
+    "url": "/gaf-metalurgica/static/js/runtime-main.d8c306bb.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/gaf-metalurgica/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/gaf-metalurgica/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/gaf-metalurgica/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/gaf-metalurgica/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/gaf-metalurgica/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/gaf-metalurgica/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/gaf-metalurgica/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/gaf-metalurgica/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/gaf-metalurgica/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/gaf-metalurgica/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/gaf-metalurgica/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/gaf-metalurgica/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/gaf-metalurgica/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/gaf-metalurgica/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/gaf-metalurgica/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/gaf-metalurgica/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/gaf-metalurgica/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
